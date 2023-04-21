37d7a9cdf6897d9bdd8305c450b1e7d85267de2a
Michel Domingos <micheldslive@gmail.com>
2023-04-21 10:48:15 -0300

    feat: adding boilerplate settings and dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import EnvironmentPlugin from "vite-plugin-environment"
+import tsConfigPaths from "vite-tsconfig-paths"
+import svgrPlugin from "vite-plugin-svgr"
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(() => ({
+  plugins: [react(), EnvironmentPlugin("all"), tsConfigPaths(), svgrPlugin()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: ".vitest/setup.ts",
+    css: true,
+  },
+}))
