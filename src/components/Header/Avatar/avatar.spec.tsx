fa8ae5ca92f2fc54b917c3ad13f87f5af611978b
Michel Domingos <micheldslive@gmail.com>
2023-04-24 09:15:17 -0300

    fix(refactor): tuning tests with router-dom routing

--- src/components/Header/Avatar/avatar.spec.tsx
+++ src/components/Header/Avatar/avatar.spec.tsx
@@ -1,11 +1,18 @@
 import { Avatar } from "."
 import { screen } from "@testing-library/react"
 import { describe, expect, test } from "vitest"
+import { createMemoryHistory } from "history"
 import { renderWithTheme } from "@/core/helpers"
+import { Router } from "react-router-dom"
 
 describe("<Avatar />", () => {
   test(`should by render a Avatar`, () => {
-    renderWithTheme(<Avatar />)
+    const history = createMemoryHistory({ initialEntries: ["/"] })
+    renderWithTheme(
+      <Router navigator={history} location={"/"}>
+        <Avatar />
+      </Router>,
+    )
     const avatar = screen.getByLabelText(/avatar/i)
     expect(avatar).toBeInTheDocument()
   })
--- src/components/Header/Profile/profile.spec.tsx
+++ src/components/Header/Profile/profile.spec.tsx
@@ -1,11 +1,18 @@
 import { Profile } from "."
 import { screen } from "@testing-library/react"
 import { describe, expect, test } from "vitest"
+import { createMemoryHistory } from "history"
 import { renderWithTheme } from "@/core/helpers"
+import { Router } from "react-router-dom"
 
 describe("<Profile />", () => {
   test(`should by render a Profile`, () => {
-    renderWithTheme(<Profile open={true} onClose={() => {}} />)
+    const history = createMemoryHistory({ initialEntries: ["/"] })
+    renderWithTheme(
+      <Router navigator={history} location={"/"}>
+        <Profile open={true} onClose={() => {}} />
+      </Router>,
+    )
     const load = screen.getByRole("dialog")
     expect(load).toBeInTheDocument()
   })
