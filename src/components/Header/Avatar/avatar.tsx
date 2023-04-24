23994402d1aaa4eecdbc4318667d76549265b825
Michel Domingos <micheldslive@gmail.com>
2023-04-24 09:22:01 -0300

    chore: adjusting import/export in header

--- src/components/Header/Avatar/avatar.tsx
+++ src/components/Header/Avatar/avatar.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 import { CircleButton, Icon } from "./styles"
-import { Profile } from "../Profile"
+import { Profile } from "@/components"
 
 export function Avatar() {
   const [open, setOpen] = useState(false)
